642b42ec4e4a8cde6c660954597d5d8955bbdfdb
Benjamin Dupuy <benjamin.dupuy8@gmail.com>
2020-05-13 14:05:47 +0200

    [MOD] Init project, config, scene and controls

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+const fs = require('fs');
+const ip = require('ip');
+const path = require('path');
+const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+
+PLUGINS = [
+  new webpack.EnvironmentPlugin(['NODE_ENV']),
+  new webpack.HotModuleReplacementPlugin(),
+  new CopyPlugin([
+    { from: './src/models', to: 'build/models' },
+    { from: './src/images', to: 'build/images' },
+  ]),
+];
+
+module.exports = {
+  devServer: {
+    disableHostCheck: true,
+    hotOnly: true,
+  },
+  entry: {
+    build: './src/index.js',
+  },
+  output: {
+    path: __dirname,
+    filename: 'build/[name].js',
+  },
+  plugins: PLUGINS,
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /(node_modules)/,
+        use: ['babel-loader', 'aframe-super-hot-loader'],
+      },
+      {
+        test: /\.html/,
+        exclude: /(node_modules)/,
+        use: [
+          'aframe-super-hot-html-loader',
+          {
+            loader: 'super-nunjucks-loader',
+            options: {
+              globals: {
+                HOST: ip.address(),
+                IS_PRODUCTION: process.env.NODE_ENV === 'production',
+              },
+              path: process.env.NUNJUCKS_PATH || path.join(__dirname, 'src'),
+            },
+          },
+          {
+            loader: 'html-require-loader',
+            options: {
+              root: path.resolve(__dirname, 'src'),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.glsl/,
+        exclude: /(node_modules)/,
+        loader: 'webpack-glsl-loader',
+      },
+      {
+        test: /\.css$/,
+        exclude: /(node_modules)/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.png|\.jpg/,
+        exclude: /(node_modules)/,
+        use: ['url-loader'],
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.join(__dirname, 'node_modules')],
+  },
+};
