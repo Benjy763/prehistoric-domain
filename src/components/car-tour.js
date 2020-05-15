31fa72d2e6b529d6b568b4b1e5a379cd808d3ae7
Benjamin Dupuy <benjamin.dupuy8@gmail.com>
2020-05-15 08:51:51 +0200

    [MOD] Update rotation system

--- src/components/car-tour.js
+++ src/components/car-tour.js
@@ -17,37 +17,39 @@ AFRAME.registerComponent('car-tour', {
       new THREE.Vector2(30, -94),
     ]);
     this.rotation = this.el.getAttribute('rotation').y;
+
+    this.updateRotation();
+  },
+  convertPosition: function (position2D) {
+    return {
+      x: position2D.x,
+      y: this.object.position.y,
+      z: position2D.y,
+    };
+  },
+  updateRotation: function () {
+    const nextMarkerForRotation = 0.0002;
+    const newPosition = this.convertPosition(
+      this.curve.getPointAt(this.data.carMarker + nextMarkerForRotation)
+    );
+    this.object.lookAt(newPosition.x, newPosition.y, newPosition.z);
+    // Correct rotation with offset
+    const rotation = this.el.getAttribute('rotation');
+    rotation.y += 88;
+    this.el.setAttribute('rotation', rotation);
   },
   tock: function () {
-    this.system.log(this.data.carMarker);
+    this.system.log(this.el.getAttribute('rotation').y);
 
     // Curve movement
     // 0.90 marker is animation ending
     if (this.data.carMarker < 0.9) {
       this.data.carMarker += this.data.carSpeed;
-      this.object.position.x = this.curve.getPointAt(this.data.carMarker).x;
-      this.object.position.z = this.curve.getPointAt(this.data.carMarker).y;
-    }
+      this.object.position.copy(
+        this.convertPosition(this.curve.getPointAt(this.data.carMarker))
+      );
 
-    // Car rotation points
-    if (this.data.carMarker > 0.33 && this.data.carMarker < 0.35) {
-      this.rotation -= 0.06;
-      this.el.setAttribute('rotation', { x: 0, y: this.rotation, z: 0 });
-    }
-
-    if (this.data.carMarker > 0.46 && this.data.carMarker < 0.48) {
-      this.rotation -= 0.08;
-      this.el.setAttribute('rotation', { x: 0, y: this.rotation, z: 0 });
-    }
-
-    if (this.data.carMarker > 0.57 && this.data.carMarker < 0.63) {
-      this.rotation -= 0.08;
-      this.el.setAttribute('rotation', { x: 0, y: this.rotation, z: 0 });
-    }
-
-    if (this.data.carMarker > 0.7 && this.data.carMarker < 0.73) {
-      this.rotation -= 0.035;
-      this.el.setAttribute('rotation', { x: 0, y: this.rotation, z: 0 });
+      this.updateRotation();
     }
   },
 });
