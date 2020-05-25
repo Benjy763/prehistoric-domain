55094c1b5e7ecc24cae44dcef3055edaa6eeb671
Benjamin Dupuy <benjamin.dupuy8@gmail.com>
2020-05-25 22:07:06 +0200

    [MOD] Switch cdn to dependencies in package json

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ function requireAll(req) {
   req.keys().forEach(req);
 }
 
+// Require libs
+require('aframe');
+require('aframe-extras');
+require('aframe-fps-counter-component');
+require('super-hands');
+require('aframe-curve-component');
+require('aframe-geometry-merger-component');
+
 // Require all components.
 require('./style/loader.css');
 requireAll(require.context('./components/', true, /\.js$/));
