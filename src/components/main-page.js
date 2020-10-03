fa9931876dea12260af0d0a4b7b3dfd2ba85feb4
Benjamin Dupuy <benjamin.dupuy8@gmail.com>
2020-10-03 23:22:11 +0200

    [ADD] Add enchors

--- src/components/main-page.js
+++ src/components/main-page.js
@@ -1,1 +1,22 @@
 let $ = require('jquery');
+
+$(document).ready(function () {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth',
+      });
+    });
+  });
+});
+
+$(document).scroll(function () {
+  var y = $(this).scrollTop();
+  if (y > 800) {
+    $('.fixed-menu').fadeIn();
+  } else {
+    $('.fixed-menu').fadeOut();
+  }
+});
