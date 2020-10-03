1186a88535ef8f895dd8b337cafd03cf9d9bc3bf
Benjamin Dupuy <benjamin.dupuy8@gmail.com>
2020-10-04 00:29:02 +0200

    [ADD] Add mecanism tickets

--- src/components/main-page.js
+++ src/components/main-page.js
@@ -1,4 +1,5 @@
 let $ = require('jquery');
+const validValues = [6971, 6972, 6973, 6974, 6975];
 $(document).ready(function () {
   document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     anchor.addEventListener('click', function (e) {
@@ -13,5 +14,13 @@ $(document).ready(function () {
   $('#get-ticket').on('click', () => {
     const randomTicket = Math.floor(Math.random() * 5) + 6971;
     window.open(`./build/images/ticket-${randomTicket}.jpg`);
+  });
+
+  $('#use-ticket').on('click', () => {
+    if (validValues.includes(Number($('#ticket-value').val()))) {
+      $('#ticket-error').css('display', 'none');
+    } else {
+      $('#ticket-error').css('display', 'block');
+    }
   });
 });
