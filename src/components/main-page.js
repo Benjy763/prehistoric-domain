1c31576ed8d2e23448bd39874bd24281fe6203de
Benjamin Dupuy <benjamin.dupuy8@gmail.com>
2020-10-04 16:58:29 +0200

    [ADD] Add functions for website js

--- src/components/main-page.js
+++ src/components/main-page.js
@@ -6,61 +6,19 @@ let trexAudio;
 let soundtrackAudio;
 
 $(document).ready(function () {
-  // Get audio assets
+  // Get assets
   turnonAudio = document.getElementById('turnon-sound');
   runpcAudio = document.getElementById('runpc-sound');
   trexAudio = document.getElementById('trex-sound');
   soundtrackAudio = document.getElementById('soundtrack-sound');
 
-  // Add scroll smooth effect
-  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
+  initLoadMecanisms();
+  initScrollSmoothEffect();
+  initTicketMecanisms();
+  initSectionsAnimations();
+});
 
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth',
-      });
-    });
-  });
-
-  // Loading mecanisms
-  $('.loading-website .title').css('opacity', '1');
-  // setTimeout(() => {
-  //   $('.loading-website .subtitle').css('opacity', '1');
-  // }, 1500);
-  setTimeout(() => {
-    $('.loading-website .title').css('opacity', '0');
-    // $('.loading-website .subtitle').css('opacity', '0');
-  }, 4000);
-  setTimeout(() => {
-    $('.loading-website').css('display', 'none');
-    $('#main-section').css('display', 'flex');
-    soundtrackAudio.play();
-  }, 5000);
-
-  // Add tickets mecanims
-  $('#get-ticket').on('click', () => {
-    const randomTicket = Math.floor(Math.random() * 5) + 6971;
-    window.open(`./build/images/ticket-${randomTicket}.jpg`);
-  });
-
-  $('#use-ticket').on('click', () => {
-    if (validValues.includes(Number($('#ticket-value').val()))) {
-      $('#ticket-error').css('display', 'none');
-      $('#main-page').css('display', 'none');
-      $('#static-loading').css('display', 'flex');
-      $('#main-scene-wrapper').css('display', 'block');
-
-      // Stop sounds
-      turnonAudio.pause();
-      runpcAudio.pause();
-      trexAudio.pause();
-      soundtrackAudio.pause();
-    } else {
-      $('#ticket-error').css('display', 'block');
-    }
-  });
-
+function initSectionsAnimations() {
   $('#main-link').on('click', () => {
     // Animations
     $('.jt-text').removeClass('slide');
@@ -119,4 +77,65 @@ $(document).ready(function () {
     turnonAudio.play();
     runpcAudio.play();
   });
-});
+}
+
+function initTicketMecanisms() {
+  // Add tickets mecanims
+  $('#get-ticket').on('click', () => {
+    const randomTicket = Math.floor(Math.random() * 5) + 6971;
+    window.open(`./build/images/ticket-${randomTicket}.jpg`);
+  });
+
+  $('#use-ticket').on('click', () => {
+    if (validValues.includes(Number($('#ticket-value').val()))) {
+      $('#ticket-error').css('display', 'none');
+      $('#main-page').css('display', 'none');
+      $('#static-loading').css('display', 'flex');
+      $('#main-scene-wrapper').css('display', 'block');
+
+      // Stop sounds
+      turnonAudio.pause();
+      runpcAudio.pause();
+      trexAudio.pause();
+      soundtrackAudio.pause();
+    } else {
+      $('#ticket-error').css('display', 'block');
+    }
+  });
+}
+
+function initScrollSmoothEffect() {
+  // Add scroll smooth effect
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth',
+      });
+    });
+  });
+}
+
+function initLoadMecanisms() {
+  // Debug
+  // $('.loading-website').css('display', 'none');
+  // $('#main-page').css('display', 'none');
+  // $('#static-loading').css('display', 'flex');
+  // $('#main-scene-wrapper').css('display', 'block');
+
+  // Loading mecanisms
+  $('.loading-website .title').css('opacity', '1');
+  // setTimeout(() => {
+  //   $('.loading-website .subtitle').css('opacity', '1');
+  // }, 1500);
+  setTimeout(() => {
+    $('.loading-website .title').css('opacity', '0');
+    // $('.loading-website .subtitle').css('opacity', '0');
+  }, 4000);
+  setTimeout(() => {
+    $('.loading-website').css('display', 'none');
+    $('#main-section').css('display', 'flex');
+    soundtrackAudio.play();
+  }, 5000);
+}
