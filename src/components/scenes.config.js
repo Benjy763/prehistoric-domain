b50676e5da45f5ccea4f26df7fe8f9a9b56b84af
Benjamin Dupuy <benjamin.dupuy8@gmail.com>
2021-12-08 15:35:22 +0100

    [MOD] Enhance dilo and raptor scene

--- src/components/scenes.config.js
+++ src/components/scenes.config.js
@@ -1,6 +1,6 @@
 export const scenes = {
   // Starting scene
-  selection: 'dilo',
+  selection: 'raptor',
   carMarkerForDebug: 0.32,
   loading: {
     scene: 'loading-scene',
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function requireAll(req) {
 // Require libs
 require('aframe');
 require('aframe-extras');
-require('aframe-fps-counter-component');
+// require('aframe-fps-counter-component');
 require('super-hands');
 require('aframe-curve-component');
 require('./vendors/aframe-gif-shader/dist/aframe-gif-component.js');
