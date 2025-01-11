d4954a516e947f4025587624191b76635c6af5c5
Benjy763 <benjamin.dupuy8@gmail.com>
2025-01-11 20:03:25 +0100

    feat: add baby to mammoth scene

--- src/components/mammoth-scene/mammoth-animation.js
+++ src/components/mammoth-scene/mammoth-animation.js
@@ -12,6 +12,7 @@ AFRAME.registerComponent('mammoth-animation', {
     this.mammoth1 = this.el;
     this.mammoth2 = document.querySelector('#mammoth-2');
     this.mammoth3 = document.querySelector('#mammoth-3');
+    this.mammothBaby = document.querySelector('#baby-mammoth');
     this.mammoth4 = document.querySelector('#mammoth-4');
     this.glacier = document.querySelector('#mammoth-glacier-1');
     this.phase = '';
@@ -40,6 +41,12 @@ AFRAME.registerComponent('mammoth-animation', {
       new THREE.Vector3(51.336, 5.6, -1.23),
       new THREE.Vector3(70, -2.575, 50)
     ]);
+    this.curveBaby = new THREE.CatmullRomCurve3([
+      new THREE.Vector3(56, -1, -60.972),
+      new THREE.Vector3(56, 4.9, -30),
+      new THREE.Vector3(56, 4.9, 10),
+      new THREE.Vector3(56, -10, 50)
+    ]);
 
     // Start tour listener
     this.el.addEventListener(
@@ -57,33 +64,39 @@ AFRAME.registerComponent('mammoth-animation', {
         this.mammoth2State = { marker: 0, enabled: false, running: false };
         this.mammoth3State = { marker: 0, enabled: false, running: false };
         this.mammoth4State = { marker: 0, enabled: false, running: false };
+        this.mammothBabyState = { marker: 0, enabled: false, running: false };
         this.glacierStateFalling = false;
         this.phaseConfig = {
           enterWalk: {
             mammoth1Speed: 0.013,
             mammoth2Speed: 0.013,
             mammoth3Speed: 0.0135,
-            mammoth4Speed: 0.012
+            mammoth4Speed: 0.012,
+            mammothBabySpeed: 0.013
           }
         };
         this.mammothSpeed = 0.003;
         this.glacierSpeed = 0.3;
         this.mammoth1.setAttribute('visible', 'true');
-        this.mammoth2.setAttribute('visible', 'true');
-        this.mammoth3.setAttribute('visible', 'true');
-        this.mammoth4.setAttribute('visible', 'true');
 
         this.mammothAudio5.playSound();
         setTimeout(() => {
-          this.mammoth2State.enabled = true;
-        }, 8000);
+          this.mammothBaby.setAttribute('visible', 'true');
+          this.mammothBabyState.enabled = true;
+        }, 7000);
         setTimeout(() => {
+          this.mammoth2.setAttribute('visible', 'true');
+          this.mammoth2State.enabled = true;
+        }, 10000);
+        setTimeout(() => {
+          this.mammoth3.setAttribute('visible', 'true');
           this.mammoth3State.enabled = true;
-        }, 12000);
+        }, 14000);
         setTimeout(() => {
           this.mammothAudio3.playSound();
         }, 18000);
         setTimeout(() => {
+          this.mammoth4.setAttribute('visible', 'true');
           this.mammoth4State.enabled = true;
         }, 25000);
         setTimeout(() => {
@@ -115,6 +128,17 @@ AFRAME.registerComponent('mammoth-animation', {
         this.curve2,
         this.mammoth2State.marker,
         this.phaseConfig[this.phase].mammoth2Speed,
+        { useDeltaTime: true }
+      );
+    }
+
+    if (this.mammothBabyState.enabled) {
+      this.mammothBabyState.marker = this.movesManager.moveOnCurve(
+        this.mammoth2State,
+        this.mammothBaby.object3D,
+        this.curveBaby,
+        this.mammothBabyState.marker,
+        this.phaseConfig[this.phase].mammothBabySpeed,
         { useDeltaTime: true }
       );
     }
@@ -182,6 +206,18 @@ AFRAME.registerComponent('mammoth-animation', {
             : this.phaseConfig[this.phase].mammoth2Speed + this.mammothSpeed;
       }, 6000);
       setTimeout(() => {
+        this.mammothBaby.setAttribute('animation-mixer', {
+          clip: 'Walk_Fast',
+          crossFadeDuration: 0.5,
+          timeScale: 1
+        });
+        this.mammothBabyState.running = true;
+        this.phaseConfig[this.phase].mammothBabySpeed =
+          this.phaseConfig[this.phase].mammothBabySpeed > 0.06
+            ? this.phaseConfig[this.phase].mammothBabySpeed
+            : this.phaseConfig[this.phase].mammothBabySpeed + this.mammothSpeed;
+      }, 6000);
+      setTimeout(() => {
         this.mammoth3.setAttribute('animation-mixer', {
           clip: 'Run',
           crossFadeDuration: 0.5,
@@ -244,7 +280,15 @@ AFRAME.registerComponent('mammoth-animation', {
             ? this.phaseConfig[this.phase].mammoth4Speed
             : this.phaseConfig[this.phase].mammoth4Speed + this.mammothSpeed;
       }
+      if (this.mammothBabyState.running) {
+        this.phaseConfig[this.phase].mammothBabySpeed =
+          this.phaseConfig[this.phase].mammothBabySpeed > 0.06
+            ? this.phaseConfig[this.phase].mammothBabySpeed
+            : this.phaseConfig[this.phase].mammothBabySpeed + this.mammothSpeed;
+      }
     }
+
+    // End of mammoth animations
     if (
       this.movesManager.truncMarker(this.mammoth1State.marker) > 950 &&
       this.mammoth1State.enabled
@@ -252,9 +296,7 @@ AFRAME.registerComponent('mammoth-animation', {
       this.mammoth1State.enabled = false;
       this.mammoth2State.enabled = false;
       this.mammoth3State.enabled = false;
-      this.mammoth1.setAttribute('visible', 'false');
-      this.mammoth2.setAttribute('visible', 'false');
-      this.mammoth3.setAttribute('visible', 'false');
+      this.mammothBabyState.enabled = false;
     }
 
     if (this.movesManager.truncMarker(this.mammoth4State.marker) > 950) {
